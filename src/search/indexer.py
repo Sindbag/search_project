cdc0eedb823034458cdac6eb25fa6e095eeaf57e
Olga <o.belickaya@softdeluxe.com>
2019-12-17 16:15:38 +0300

    Realization add_document

--- src/search/indexer.py
+++ src/search/indexer.py
@@ -1,4 +1,6 @@
 from .document import Document
+from collections import defaultdict
+import codecs
 
 index = []
 
@@ -20,3 +22,45 @@ def build_index():
         'Я променял девичий смех\nНа голос лектора занудный,',
         'path_3'
     ))
+
+
+def check(sym):
+    return ('a' <= sym <= 'z' or
+            'A' <= sym <= 'Z' or
+            'а' <= sym <= 'я' or
+            'А' <= sym <= 'Я' or
+            '0' <= sym <= '9')
+
+
+def words_clear(words):
+    words_del = []
+    curr_word = ""
+    for sym in words:
+        if check(sym):
+            curr_word += sym
+        else:
+            if len(curr_word) > 2:
+                words_del += [curr_word]
+            curr_word = ""
+    if len(curr_word) > 2:
+        words_del += [curr_word]
+    return words_del
+
+
+class InvIndex:
+    def __init__(self):
+        self.dictionary = defaultdict(set)
+        self.documents = []
+
+    def add_document(self, path):
+        file = codecs.open(path, 'r', "utf-8")
+        text = file.read()
+        self.documents += [Document(path, path, path)]
+        for word in words_clear(text):
+            self.dictionary[word].add(len(self.documents) - 1)
+
+    def get(self, word):  # ->list
+        pass
+
+    def get_all(self, query):
+        pass
