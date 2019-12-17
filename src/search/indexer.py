9e4572a5e2360203e9acb29bd921b36a50dfcf4b
Olga <o.belickaya@softdeluxe.com>
2019-12-17 16:38:05 +0300

    Realization add_document

--- src/search/indexer.py
+++ src/search/indexer.py
@@ -2,27 +2,6 @@ from .document import Document
 from collections import defaultdict
 import codecs
 
-index = []
-
-
-def build_index():
-    # Считывает данные и строит индекс
-    index.append(Document(
-        'The Beatles — Come Together',
-        'Here come old flat top\nHe come groovin\' up slowly',
-        'path_1'
-    ))
-    index.append(Document(
-        'The Rolling Stones — Brown Sugar',
-        'Gold Coast slave ship bound for cotton fields\nSold in the market down in New Orleans',
-        'path_2'
-    ))
-    index.append(Document(
-        'Физтех — Я променял девичий смех',
-        'Я променял девичий смех\nНа голос лектора занудный,',
-        'path_3'
-    ))
-
 
 def check(sym):
     return ('a' <= sym <= 'z' or
@@ -64,3 +43,25 @@ class InvIndex:
 
     def get_all(self, query):
         pass
+
+
+index = []
+
+
+def build_index():
+    # Считывает данные и строит индекс
+    index.append(Document(
+        'The Beatles — Come Together',
+        'Here come old flat top\nHe come groovin\' up slowly',
+        'path_1'
+    ))
+    index.append(Document(
+        'The Rolling Stones — Brown Sugar',
+        'Gold Coast slave ship bound for cotton fields\nSold in the market down in New Orleans',
+        'path_2'
+    ))
+    index.append(Document(
+        'Физтех — Я променял девичий смех',
+        'Я променял девичий смех\nНа голос лектора занудный,',
+        'path_3'
+    ))
