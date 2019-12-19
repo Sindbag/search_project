04b62a88797032b36dd4276404b90f11be41f58e
Olga <o.belickaya@softdeluxe.com>
2019-12-19 11:13:10 +0300

    Realization of add_document function and fixes

--- src/search/indexer.py
+++ src/search/indexer.py
@@ -63,7 +63,20 @@ class InvIndex:
 index = InvIndex()
 
 
-def build_index(path):
+def build_index():
     # Считывает данные и строит индекс
-    for file in os.listdir(path):
-        index.add_document(path + "/" + file)
+    index.append(Document(
+        'The Beatles — Come Together',
+        'Here come old flat top\nHe come groovin\' up slowly',
+        'path_1'
+    ))
+    index.append(Document(
+        'The Rolling Stones — Brown Sugar',
+        'Gold Coast slave ship bound for cotton fields\nSold in the market down in New Orleans',
+        'path_2'
+    ))
+    index.append(Document(
+        'Физтех — Я променял девичий смех',
+        'Я променял девичий смех\nНа голос лектора занудный,',
+        'path_3'
+    ))
