67b6cf8b290242b8b48a37d55788344c17b244df
qazxswqazxsw1 <aleonov@fmschool72.ru>
2019-12-19 12:51:40 +0600

    Fixed variable names

--- src/search/indexer.py
+++ src/search/indexer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .document import Document
-from collections import defaultdict
-import codecs
-
 from search.document import Document
 from collections import defaultdict
 from search.query import Query
+import codecs
 
 def normal_word(word):
     res = ""
@@ -40,26 +37,30 @@ class InvIndex:
         return self.dictionary[word]
       
     def get_all(self, query):
-        dict_doc = {} # dict_doc[индекс документа] = как часто встречается  
+        doc_freqs = {}  
         
         for word in query.get_clean():
-            list_index_document = self.get(word) # множество индексов документов в которых встречается слова из запроса
-            for index_document in list_index_document:
-                if index_document not in dict_doc: # проверка присутствия индекса документа в словаре(dict_doc)
-                    dict_doc[index_document] = 1 
+            word_doc_idxs = self.get(word)
+            for index_document in word_doc_idxs:
+                if index_document not in doc_freqs:
+                    doc_freqs[index_document] = 1 
                 else:
-                    dict_doc[index_document] = dict_doc[index_document] + 1
-        result = [] # список [индекс документа, как часто встречается документ по запросу каждого слова(суммарно)]
-        for index_document, value in dict_doc.items():
-            result.append((index_document, value))
-        result = sorted(result, key=lambda x: (-x[1], x[0])) # сортируем наш список в порядке убывания значения, затем по индексу документа
-        answer = []
-        for i, j in result:
-            answer.append(i)
-        return answer
+                    doc_freqs[index_document] = doc_freqs[index_document] + 1
+        doc_freq_pairs = []
+        for index_document, value in doc_freqs.items():
+            doc_freq_pairs.append((index_document, value))
+        doc_freq_pairs = sorted(doc_freq_pairs, key=lambda x: (-x[1], x[0])) 
+        result = []
+        for doc, freq in doc_freq_pairs:
+            result.append(doc)
+        return result
 
-def build_index():
+def build_index(path):
     # Считывает данные и строит индекс
+    
+    for file in os.listdir(path):
+        index.add_document(path + "/" + file)
+    
     index.append(Document(
         'The Beatles — Come Together',
         'Here come old flat top\nHe come groovin\' up slowly',
