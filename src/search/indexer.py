a0a1b8388b7540e887f9bce257363ef8f84f4226
staf02 <58979242+staf02@users.noreply.github.com>
2019-12-19 10:16:28 +0300

    ------

--- src/search/indexer.py
+++ src/search/indexer.py
@@ -1,65 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .document import Document
-from collections import defaultdict
-import codecs
-
 from search.document import Document
 from collections import defaultdict
 from search.query import Query
+import codecs
+import os
 
-def normal_word(word):
-    res = ""
-    arr = []
-    for c in word:
+
+def split_query(query):
+    word = ""
+    words = []
+    for c in query:
         if 'A' <= c <= 'Z' or 'a' <= c <= 'z' or '0' <= c <= '9' or 'А' <= c <= 'Я' or 'а' <= c <= 'я':
-            res += c
+            word += c
         else:
-            if res != "":
-                arr.append(res)
-            res = ""
-    if (res != ""):
-        arr.append(res)
-    return arr
+            if word != "" and len(word) > 2:
+                words.append(word)
+            word = ""
+    if (word != "" and len(word) > 2):
+        words.append(word)
+    return words
 
 
 class InvIndex:
     def __init__(self):
         self.dictionary = defaultdict(set)
         self.documents = []
-        
+
     def add_document(self, path):
         f = codecs.open(path, "r", "utf-8")
         text = f.read()
-        arr = normal_word(text);
-        for s in arr:
+        words = split_query(text);
+        for s in words:
             self.dictionary[s].add(Document(path, path, path))
-    
+            self.documents.append(Document(path, path, path))
+
     def get(self, word):
         return self.dictionary[word]
-      
-    def get_all(self, query):
-        dict_doc = {} # dict_doc[индекс документа] = как часто встречается  
-        
-        for word in query.get_clean():
-            list_index_document = self.get(word) # множество индексов документов в которых встречается слова из запроса
-            for index_document in list_index_document:
-                if index_document not in dict_doc: # проверка присутствия индекса документа в словаре(dict_doc)
-                    dict_doc[index_document] = 1 
-                else:
-                    dict_doc[index_document] = dict_doc[index_document] + 1
-        result = [] # список [индекс документа, как часто встречается документ по запросу каждого слова(суммарно)]
-        for index_document, value in dict_doc.items():
-            result.append((index_document, value))
-        result = sorted(result, key=lambda x: (-x[1], x[0])) # сортируем наш список в порядке убывания значения, затем по индексу документа
-        answer = []
-        for i, j in result:
-            answer.append(i)
-        return answer
 
-def build_index():
+    def get_all(self, query):
+        doc_freqs = {}
+
+        for word in split_query(query):
+            word_doc_idxs = self.get(word)
+            for index_document in word_doc_idxs:
+                if index_document not in doc_freqs:
+                    doc_freqs[index_document] = 1
+                else:
+                    doc_freqs[index_document] = doc_freqs[index_document] + 1
+        doc_freq_pairs = []
+        for index_document, value in doc_freqs.items():
+            doc_freq_pairs.append((index_document, value))
+        doc_freq_pairs = sorted(doc_freq_pairs, key=lambda x: (-x[1], x[0]))
+        result = []
+        for doc, freq in doc_freq_pairs:
+            result.append(doc)
+        return result
+
+index = InvIndex()
+
+def build_index(path):
     # Считывает данные и строит индекс
+
+    for file in os.listdir(path):
+        index.add_document(path + "/" + file)
+
     index.append(Document(
         'The Beatles — Come Together',
         'Here come old flat top\nHe come groovin\' up slowly',
